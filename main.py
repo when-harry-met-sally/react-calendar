54ffa7648071eedde7c1a40706afc36e31d31a9e
Miles <miles-moran@hotmail.com>
2020-04-05 12:09:45 -0500

    working calc

---
+++ main.py
@@ -0,0 +1,65 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from pprint import pprint
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def retrieveEvents():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    calendar_result = service.calendarList().list().execute()
+    all_calendars = calendar_result['items']
+
+    calendars = ['Fort Moran' , 'Fort Guac']
+    events = []
+    for calendar_list_entry in all_calendars:
+        cal = calendar_list_entry
+        name = cal['summary']
+        if name in calendars:
+            calId = cal['id']
+            cal_events = service.events().list(calendarId = calId).execute()
+            for e in cal_events['items']:
+                events.append(e)
+    return events
+
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route('/')
+def getEvents():
+    events = retrieveEvents()
+    events = jsonify(events)
+    return events
